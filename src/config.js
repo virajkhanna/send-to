61293ce3e38e373e3e552b69c279ecf99d238a39
virajkhanna <68863014+virajkhanna@users.noreply.github.com>
2021-06-09 15:13:49 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBGk_BiU0dFBgUlWhXuq1VOH5Zn03YF_xs",
-  authDomain: "sendto-database.firebaseapp.com",
-  databaseURL: "https://sendto-database-default-rtdb.firebaseio.com",
-  projectId: "sendto-database",
-  storageBucket: "sendto-database.appspot.com",
-  messagingSenderId: "347730350992",
-  appId: "1:347730350992:web:6cd7a8f5f60f59fe705915",
-  measurementId: "G-0HVWTZSX0B"
+  apiKey: "*******************************",
+  authDomain: "*******************************",
+  databaseURL: "*******************************",
+  projectId: "*******************************",
+  storageBucket: "*******************************",
+  messagingSenderId: "*******************************",
+  appId: "*******************************",
+  measurementId: "*******************************"
 };
 
 firebase.initializeApp(firebaseConfig);
